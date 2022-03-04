253588da623f8b4f7168d9eed76aacc97ba9165e
João <sousajoaopedro1@gmail.com>
2022-03-03 22:17:27 -0300

    finalizado curso com corretor

--- corretor.py
+++ corretor.py
@@ -16,12 +16,21 @@ def normalizacao(lista_palavras):
     return lista_normalizada
 
 
+def gerador_turbinado(palavras_geradas):
+    novas_palavras = []
+    for palavra in palavras_geradas:
+        novas_palavras += gerador_palavras(palavra)
+    return novas_palavras
+
+
 def gerador_palavras(palavra):
     fatias = []
     for i in range(len(palavra) + 1):
         fatias.append((palavra[:i], palavra[i:]))
     palavras_geradas = insere_letras(fatias)
     palavras_geradas += deletando_caracteres(fatias)
+    palavras_geradas += troca_letras(fatias)
+    palavras_geradas += inverte_letras(fatias)
 
     return palavras_geradas
 
@@ -40,6 +49,36 @@ def deletando_caracteres(fatias):
     for E, D in fatias:
         novas_palavras.append(E + D[1:])
     return novas_palavras
+
+
+def troca_letras(fatias):
+    novas_palavras = []
+    letras = 'abcdefghijklmnopqrstuvwxyzàáâãèéêìíîòóôõùúûç'
+    for E, D in fatias:
+        for letra in letras:
+            novas_palavras.append(E + letra + D[1:])
+    return novas_palavras
+
+
+def inverte_letras(fatias):
+    novas_palavras = []
+    for E, D in fatias:
+        if len(D) > 1:
+            novas_palavras.append(E + D[1] + D[0] + D[2:])
+    return novas_palavras
+
+
+def novo_corretor(palavra, vocabulario):
+    palavras_geradas = gerador_palavras(palavra)
+    palavras_turbinadas = gerador_turbinado(palavras_geradas)
+    todas_palavras = set(palavras_turbinadas + palavras_geradas)
+    candidatos = [palavra]
+    for palavra in todas_palavras:
+        if palavra in vocabulario:
+            candidatos.append(palavra)
+
+    palavra_correta = max(candidatos, key=probabilidade)
+    return palavra_correta
 
 
 def corretor(palavra):
@@ -62,18 +101,6 @@ def cria_dados_testes(nome_arquivo):
     return lista_palavras_teste
 
 
-def avaliador(testes):
-    numero_palavras = len(testes)
-    acertou = 0
-    for correta, errada in testes:
-        palavra_corrigida = corretor(errada)
-        if palavra_corrigida == correta:
-            acertou += 1
-
-    taxa_acerto = round(acertou * 100 / numero_palavras, 2)
-    print(f'{taxa_acerto}% de {numero_palavras} palavras')
-
-
 with open('artigos.txt', "r", encoding='utf-8') as f:
     artigos = f.read()
 
@@ -83,6 +110,8 @@ lista_normalizada = normalizacao(lista_palavras)
 total_palavras = len(set(lista_normalizada))
 frequencia = nltk.FreqDist(lista_normalizada)
 
-palavra_exemplo = 'lgica'
+palavra_exemplo = 'lgóica'
+palavras_geradas = gerador_palavras(palavra_exemplo)
+print(palavras_geradas)
 correcao = corretor(palavra_exemplo)
 print(correcao)
